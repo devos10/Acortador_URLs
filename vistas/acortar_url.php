d62c470a5ccbaa2d45aa362f26e92683b7437131
devos10 <osbaldo081004@gmail.com>
2025-09-02 13:37:24 -0600

    cambiar el nombre del archivo al que va a llegar la informacion

--- vistas/acortar_url.php
+++ vistas/acortar_url.php
@@ -22,7 +22,7 @@
         </div>
 
 
-      <form class="card p-4 shadow border-0" method="GET" action="../php/procesar_url.php" style="background:#111827"
+      <form class="card p-4 shadow border-0" method="GET" action="../php/acortar_url.php" style="background:#111827"
       hx-target="#resultado" hx-swap="innerHTML">
     <div class="mb-3">
         <label class="form-label text-white">URL larga</label>
