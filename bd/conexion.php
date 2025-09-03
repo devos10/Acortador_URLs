ee5c555da668f5e0cfa2e4c47fbede78438ec55b
devos10 <osbaldo081004@gmail.com>
2025-09-03 11:27:39 -0600

    agregando la conexion a la base de datos

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,1 +1,18 @@
 <?php
+class Conexion {
+    protected $conexion;
+
+    public function __construct() {
+        $host = 'localhost';
+        $db   = 'urls';
+        $user = 'root';
+        $password = '100408ovc';
+
+        try {
+            $this->conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
+            $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
