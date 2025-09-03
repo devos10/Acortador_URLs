cd72fc58d4dbd9393f8f5c7d28c490fd667d1c12
devos10 <osbaldo081004@gmail.com>
2025-09-03 12:24:20 -0600

    creamos la funcion para insertar en la tabla url

--- clases/url.php
+++ clases/url.php
@@ -1,2 +1,12 @@
 <?php
+include "bd/conexion.php";
 /* */
+
+class Url extends Conexion{
+    public function insertarUrlCorta($url_larga,$url_hash,$url_corta,$expiracion){
+        $sql= $this->conexion->prepare(
+            "INSERT INTO url (url_larga, url_hash, url_corta) VALUES (?,?,?,?)");
+        return $sql->execute([$url_larga, $url_hash,$url_corta,$expiracion]);
+
+    }
+}
